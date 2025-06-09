6cc9a250fc8f98c728f6169e04e1eda417bd90a8
tulipoaaaaa <158385086+tulipoaaaaa@users.noreply.github.com>
2025-06-09 18:13:40 +0100

    feat: add 'Rebalance Now' button to DashboardTab with signal

--- CorpusBuilderApp/tests/ui/test_dashboard_tab.py
+++ CorpusBuilderApp/tests/ui/test_dashboard_tab.py
@@ -26,3 +26,14 @@ def test_view_all_activity_signal(dashboard_tab, qtbot):
     dashboard_tab.view_all_activity_requested.connect(on_request)
     qtbot.mouseClick(dashboard_tab.view_all_btn, Qt.MouseButton.LeftButton)
     assert triggered
+
+
+def test_rebalance_now_signal(dashboard_tab, qtbot):
+    triggered = []
+
+    def on_request():
+        triggered.append(True)
+
+    dashboard_tab.rebalance_requested.connect(on_request)
+    qtbot.mouseClick(dashboard_tab.rebalance_now_btn, Qt.MouseButton.LeftButton)
+    assert triggered
